df67d3a446e14981ab2e88ef65b999e06a3a8a9b
“YOUR <2drum4fun@gmail.com>
2019-03-02 21:46:15 -0600

    add all contact with second url

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -1,6 +1,11 @@
 class Api::ContactsController < ApplicationController
   def index
-    @contact = Contact.last
+    @contact = Contact.first
     render 'index.json.jbuilder'
+  end
+
+  def indices
+    @contacts = Contact.all
+    render 'indices.json.jbuilder'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
     get "/contact" => "contacts#index"
+    get "/all_contacts" => "contacts#indices"
   end
 end
